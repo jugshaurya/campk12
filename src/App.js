938762dc92eeee292d758eb4c14a4b6e6f0cee63
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-05-13 12:50:44 +0530

    :basic background and navbar addition:

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="BG_Header">
+        <div className="wrapper">
+          <nav>
+            <div className="CampK12-Logo"></div>
+            <div className="Leaderboard"></div>
+            <div className="Group-36"></div>
+            <div className="Rectangle-17">
+              <span className="Skill-Icon"></span>
+              <span className="class-100">100</span>
+              <span className="Bitmap"></span>
+            </div>
+            <div className="Shape"></div>
+          </nav>
+        </div>
+        <div className="Rectangle-24"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
