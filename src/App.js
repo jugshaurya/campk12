ade661c8c89a502f26cd08487d74e68461ee41ca
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-05-14 15:48:45 +0530

    :lion: Routing added for page1 ad page2 navigation.

--- src/App.js
+++ src/App.js
@@ -1,65 +1,27 @@
 import React from "react";
 import "./App.scss";
-import ShowCardsSection from "./ShowCardsSection";
-import ShowSliderSection from "./ShowSliderSection";
-import { ReactComponent as Coin } from "./assets/camp-k-12-coins-copy-2.svg";
+import { Switch, Route, Link } from "react-router-dom";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="BG_Header">
-        <div className="wrapper">
-          <nav>
-            <div className="CampK12-Logo"></div>
-            <div className="Leaderboard"></div>
-            <div className="Group-36">
-              <div className="Oval">2</div>
-            </div>
-            <div className="Rectangle-17">
-              <span className="Skill-Icon"></span>
-              <span className="class-100">100</span>
-              <span className="Bitmap"></span>
-            </div>
-            <div className="Shape"></div>
-          </nav>
-
-          <header>
-            <div className="left">
-              <div className="Welcome-to-the-school">
-                Welcome to the school of the future.
-              </div>
-              <div className="Cutting-edge-technol">
-                Cutting-edge technology courses for Ages 8 - 18. Unlock{" "}
-                <span className="text-style-1">21st Century skills</span>, earn
-                <span className="CampK12Coins-Copy-2">
-                  <Coin className="CampK12Coins-Copy-2" />
-                </span>
-                , and build a college-ready portfolio as you learn.
-              </div>
-            </div>
-            <div className="right">
-              <div className="Header-Photo"></div>
-            </div>
-          </header>
-          <main>
-            <section className="one">
-              <ShowSliderSection />
-            </section>
-            <section className="two">
-              <ShowCardsSection />
-            </section>
-          </main>
-        </div>
-        <footer className="Rectangle">
-          <span className="Questions-Contact-u">
-            Questions? Contact us at{" "}
-            <span className="text-style-1">info@campk12.com</span>
-          </span>
-        </footer>
-        {/* <div className="Rectangle-24"></div> */}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Link to="/" className="popup a">
+          Go to Page 1{" "}
+        </Link>
+        <Link to="/page2" className="popup b">
+          Go to Page 2{" "}
+        </Link>
+        <Switch>
+          <Route exact path="/" component={Page1} />
+          <Route exact path="/page2" component={Page2} />
+          <Route path="/" component={Page1} />
+        </Switch>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Page1.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ShowCardsSection from "./ShowCardsSection";
+import ShowSliderSection from "./ShowSliderSection";
+import { ReactComponent as Coin } from "./assets/camp-k-12-coins-copy-2.svg";
+import "./Page1.scss";
+const Page1 = () => {
+  return (
+    <div className="Page1 BG_Header">
+      <div className="wrapper">
+        <nav>
+          <div className="CampK12-Logo"></div>
+          <div className="Leaderboard"></div>
+          <div className="Group-36">
+            <div className="Oval">2</div>
+          </div>
+          <div className="Rectangle-17">
+            <span className="Skill-Icon"></span>
+            <span className="class-100">100</span>
+            <span className="Bitmap"></span>
+          </div>
+          <div className="Shape"></div>
+        </nav>
+
+        <header>
+          <div className="left">
+            <div className="Welcome-to-the-school">
+              Welcome to the school of the future.
+            </div>
+            <div className="Cutting-edge-technol">
+              Cutting-edge technology courses for Ages 8 - 18. Unlock{" "}
+              <span className="text-style-1">21st Century skills</span>, earn
+              <span className="CampK12Coins-Copy-2">
+                <Coin className="CampK12Coins-Copy-2" />
+              </span>
+              , and build a college-ready portfolio as you learn.
+            </div>
+          </div>
+          <div className="right">
+            <div className="Header-Photo"></div>
+          </div>
+        </header>
+        <main>
+          <section className="one">
+            <ShowSliderSection />
+          </section>
+          <section className="two">
+            <ShowCardsSection />
+          </section>
+        </main>
+      </div>
+      <footer className="Rectangle">
+        <span className="Questions-Contact-u">
+          Questions? Contact us at{" "}
+          <span className="text-style-1">info@campk12.com</span>
+        </span>
+      </footer>
+    </div>
+  );
+};
+
+export default Page1;
